cc600b62163db5dd423abe2f16194b5feecb60d3
Evgeny Vais <vais.evgeny@gmail.com>
2013-08-30 16:58:48 +0600

    Added exception handling in the main function

--- src/users/users.cpp
+++ src/users/users.cpp
@@ -24,7 +24,7 @@ int main(int argc,char* argv[]){
   try
   {
     locale::global(std::locale(""));
-    if(argc < 2) { cerr << "ERR: Too few arguments\n"; exit(1); }
+    if(argc < 2) throw invalid_argument("Too few arguments");
 
     int cur_arg = 0;
     bool global_args = true;
@@ -57,8 +57,7 @@ int main(int argc,char* argv[]){
           config_path = argv[++cur_arg];
           break;
         default:
-          cerr << "ERR: Unknown command: " << argv[cur_arg] << endl;
-          exit (1);
+          throw invalid_argument("Unknown command");
       }
       if(argc <= cur_arg+1) break;
     }
@@ -93,30 +92,62 @@ int main(int argc,char* argv[]){
     stringstream message;
     map <string,int> excp;
 
-    excp["User with that UID does't exist"]				= 201;
-    excp["User with such name is already exist"]		= 202;
-    excp["User with such MAC address is already exist"]	= 203;
+    excp["Too few arguments"]						= 100;
+    excp["Unknown command"]							= 101;
+    excp["MAC address is not correctly entered"]	= 102;
+    excp["Null name"]								= 103;
+    excp["UID is not valid"]						= 104;
+    excp["UID required"]							= 105;
+
+    excp["User with that UID does't exist"]				= 200;
+    excp["User with such name is already exist"]		= 201;
+    excp["User with such MAC address is already exist"]	= 202;
 
     message << "ERR ";
     code = excp[e.what()];
     switch(code)
     {
-      case 201:
+      case 100:
+        message << code << ": "
+                << _("Too few arguments");
+        break;
+      case 101:
+        message << code << ": "
+                << _("Unknown command");
+        break;
+      case 102:
+        message << code << ": "
+                << _("MAC address is not correctly entered");
+        break;
+      case 103:
+        message << code << ": "
+                << _("Null name");
+        break;
+      case 104:
+        message << code << ": "
+                << _("UID is not valid");
+        break;
+      case 105:
+        message << code << ": "
+                << _("UID required");
+        break;
+
+      case 200:
         message << code << ": "
                 << _("User with that UID does't exist");
         break;
-      case 202:
+      case 201:
         message << code << ": "
                 << _("User with such name is already exist");
         break;
-      case 203:
+      case 202:
         message << code << ": "
                 << _("User with such MAC address is already exist");
         break;
       default:
         message << ++code << ": "
                 << "Non provided exception: "
-                << e.what() << endl;
+                << e.what();
     }
     cerr << message.str() << endl;
 
@@ -175,26 +206,25 @@ void add(int argc,char **argv,seacl::acl &users){
         break;
       case 'm':
         mac.parse(optarg);
-        if(mac.empty()) { cerr << "ERR: MAC address is not correctly entered: " << optarg << endl; exit(1); }
+        if(mac.empty()) throw invalid_argument("MAC address is not correctly entered");
         break;
     }
   }
 
-  if(name == "") { cerr << "ERR: Null name\n"; exit(1); }
+  if(name == "") throw invalid_argument("Null name");
   uid = users.add(gid,name);
   if(!mac.empty())
     users.mod_eui48(uid,mac.str());
 }
 
 void del(int argc,char **argv,seacl::acl &users){
-  if(argc < 2) { cerr << "ERR: Too few arguments\n"; exit(1); }
+  if(argc < 2) throw invalid_argument("Too few arguments");
 
   int uid = atoi(argv[1]);
   if(uid)
     users.del(uid);
-  else{
-    cerr << "ERR: UID is not valid: " << argv[1] << endl;
-    exit(1);}
+  else
+    throw invalid_argument("UID is not valid");
 }
 
 void mod(int argc,char **argv,seacl::acl &users){
@@ -216,7 +246,7 @@ void mod(int argc,char **argv,seacl::acl &users){
     switch(arg){
       case 'u':
         uid = atoi(optarg);
-        if(!uid) { cerr << "ERR: UID is not valid: " << optarg << endl; exit(1); }
+        if(!uid) throw invalid_argument("UID is not valid");
         break;
       case 'g':
         gid = atoi(optarg);
@@ -226,13 +256,12 @@ void mod(int argc,char **argv,seacl::acl &users){
         break;
       case 'm':
         mac.parse(optarg);
-        if(mac.empty()) { cerr << "ERR: MAC address is not correctly entered: " << optarg << endl; exit(1); }
+        if(mac.empty()) throw invalid_argument("MAC address is not correctly entered");
         break;
     }
   }
 
-  if(!uid) { cerr << "ERR: UID required\n"; exit(1); }
-  if(!users.exist(uid)) { cerr << "ERR: User with UID " << uid << " does not exist\n"; exit(1); }
+  if(!uid) throw invalid_argument("UID required");
   if(gid == -1) gid = users.gid(uid);
   if(name == "") name = users.username(uid);
 
